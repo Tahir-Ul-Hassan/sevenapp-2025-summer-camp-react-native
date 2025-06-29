8c7530a7e7feb0b129f65d188b7eaf60f1d4be01
MahrTahirulhassan <mahrtahirulhassan@gmail.com>
2025-06-29 10:25:29 +0500

    horizontal concept with item number

--- src/screens/listing.js
+++ src/screens/listing.js
@@ -1,28 +1,36 @@
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  ScrollView,
+} from "react-native";
 import React from "react";
 
 export default function Listing() {
   const data = [1, 2, 3, 4, 5];
 
   return (
-    <ScrollView>
-      <View>
-        {data.map((item, index) => (
-          <Image
-            key={index}
-            style={styles.img}
-            source={require("../../assets/image.png")}
-          />
-        ))}
-      </View>
+    <ScrollView horizontal={true}>
+      {data.map((item) => (
+        <ImageBackground
+          style={styles.img}
+          source={require("../../assets/image.png")}
+        >
+          <Text style={styles.textStyle}>{item}</Text>
+        </ImageBackground>
+      ))}
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   img: {
-    width: "100%",
-    height: 200,
-    marginVertical: 20,
+    width: 300,
+    height: 300,
+  },
+  textStyle: {
+    fontSize: 40,
+    color: "white",
   },
 });
